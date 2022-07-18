e02af666f5423ac622a94b8ffb803b384cdff558
davidvajda <davidvajda1998@gmail.com>
2022-07-18 18:43:05 +0200

    fixed dimension panel animation

--- src/Components/PanelDimension/PanelDimension.js
+++ src/Components/PanelDimension/PanelDimension.js
@@ -1,29 +1,9 @@
-import React, { useState, useRef, useContext } from "react";
+import React, { useState, useContext } from "react";
 
 import { MaterialStateContext } from "../../ContextProvider/ContextProvider";
 
-const PANEL_CLASS = {
-  IDLE: "dimension-panel dimension-panel-animation",
-  HOVER: "dimension-panel dimension-panel-animation-hover",
-};
-
 export const PanelDimensions = () => {
-  const [className, setClassName] = useState(() => PANEL_CLASS.IDLE);
-  const [animationFinished, setAnimationFinished] = useState(() => true);
-  const [isHovered, setIsHovered] = useState(() => false);
   const { materialState } = useContext(MaterialStateContext);
-
-  const panelRef = useRef();
-
-  const protectAnimation = () => {
-    setAnimationFinished(false);
-    setTimeout(() => {
-      setAnimationFinished(true);
-      if (isHovered) {
-        setClassName(PANEL_CLASS.IDLE);
-      }
-    }, 1000);
-  };
 
   let w = materialState.panelWidth;
   let h = materialState.panelHeight;
@@ -50,25 +30,7 @@ export const PanelDimensions = () => {
   };
 
   return (
-    <div
-      ref={panelRef}
-      style={styles.panel}
-      className={className}
-      onMouseOver={() => {
-        setIsHovered(true);
-        if (animationFinished) {
-          setClassName(PANEL_CLASS.HOVER);
-          protectAnimation();
-        }
-      }}
-      onMouseLeave={() => {
-        setIsHovered(false);
-        if (animationFinished) {
-          setClassName(PANEL_CLASS.IDLE);
-          protectAnimation();
-        }
-      }}
-    >
+    <div style={styles.panel} className="dimension-panel">
       <div style={styles.widthDimension} className="dimension-panel-dimmension">
         {w} mm
       </div>
